6a09caef6366006161f83bc6741d465ef31280ad
Nikola <40897274+0nikola1@users.noreply.github.com>
2023-06-15 23:21:23 +0200

    Update get.sh

--- get.sh
+++ get.sh
@@ -2,15 +2,25 @@
 
 REPO_URL="https://github.com/roomsensr/install.git"
 DESTINATION_FOLDER="packages"
+DESTINATION_FOLDER2="pyscript"
 
 # Clone the repository
 git clone $REPO_URL temp_dir
 
-# Check if the destination folder exists
+# Move to the first destination folder if it exists or create it
 if [ -d "$DESTINATION_FOLDER" ]; then
     mv temp_dir/packages/* $DESTINATION_FOLDER
 else
-    mv temp_dir .
+    mkdir $DESTINATION_FOLDER
+    mv temp_dir/packages/* $DESTINATION_FOLDER
+fi
+
+# Move to the second destination folder if it exists or create it
+if [ -d "$DESTINATION_FOLDER2" ]; then
+    mv temp_dir/pyscript/* $DESTINATION_FOLDER2
+else
+    mkdir $DESTINATION_FOLDER2
+    mv temp_dir/pyscript/* $DESTINATION_FOLDER2
 fi
 
 # Cleanup - remove the temporary directory
