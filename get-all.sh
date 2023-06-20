d61545e941d7ab7ce5a25512c66b45d1c1ac75dd
Nikola <40897274+0nikola1@users.noreply.github.com>
2023-06-20 17:17:11 +0200

    Update get-all.sh

--- get-all.sh
+++ get-all.sh
@@ -4,7 +4,8 @@ REPO_URL="https://github.com/roomsensr/install.git"
 DESTINATION_FOLDER="packages"
 DESTINATION_FOLDER2="pyscript"
 DESTINATION_FOLDER3="www/cards"
-
+DESTINATION_FOLDER4="custom_components/pyscript"
+PYSCRIPT_RELEASE_URL="https://github.com/custom-components/pyscript/releases/download/1.4.0/hass-custom-pyscript.zip"
 # Clone the repository
 git clone $REPO_URL temp_dir
 
@@ -24,6 +25,7 @@ else
     mv temp_dir/pyscript/* $DESTINATION_FOLDER2
 fi
 
+# Move card destination folder if it exists or create it
 if [ -d "$DESTINATION_FOLDER3" ]; then
     mv temp_dir/cards/* $DESTINATION_FOLDER3
 else
@@ -34,5 +36,14 @@ else
     mv temp_dir/cards/* $DESTINATION_FOLDER3
 fi
 
+
+if [ -d "$DESTINATION_FOLDER4" ]; then
+    :
+else
+    mkdir -p "$DESTINATION_FOLDER4"
+    wget "$PYSCRIPT_RELEASE_URL" -O pyscript-release.zip
+    unzip pyscript-release.zip -d "$DESTINATION_FOLDER4"
+    rm pyscript-release.zip
+fi
 # Cleanup - remove the temporary directory
 rm -rf temp_dir
